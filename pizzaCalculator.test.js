64d332b29efaff27c0ea791c2b63afdfc51854b7
Lucas <lucasburgosloyola@gmail.com>
2026-03-31 09:38:08 -0300

    Fix del calculador

--- pizzaCalculator.test.js
+++ pizzaCalculator.test.js
@@ -1,7 +1,7 @@
 const calculateWater = require('./pizzaCalculator');
 
 test('Calcula el agua para 1000g de harina al 65% de hidratación', () => {
-  expect(calculateWater(1000, 65)).toBe(999);
+  expect(calculateWater(1000, 65)).toBe(650);
 });
 
 test('Calcula el agua para 500g de harina al 70% de hidratación', () => {
